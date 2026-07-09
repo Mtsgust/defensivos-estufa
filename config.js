7f8f1d48475b8cffbc9963f64f24b3d2aac81818
Mtsgust <ff8765io@gmail.com>
2026-07-09 17:37:08 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+// Cole aqui a URL do seu App da Web do Google Apps Script (termina em /exec)
+const API_URL = 'https://script.google.com/macros/s/AKfycbyuCWTKj_datn4uplxF_pjTiFPN8PqySzi8Y3GLd-3qEzS9Rfg6Qoq68WAmcSbdov0t/exec';
